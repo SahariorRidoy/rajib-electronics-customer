744efcd823642db612ba222ad0940d9d3baf9e5b
Saharior Ridoy <emonhridoy2014@gmail.com>
2026-06-13 23:00:49 +0600

    fix issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,6 @@ import Script from "next/script";
 import ToastProvider from "@/components/ToastProvider";
 import ReduxProvider from "../store/ReduxProvider";
 import ClientLayout from "@/components/ClientLayout";
-import MetaPixel from "@/components/MetaPixel";
-
-const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID ?? "1300284518118504";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -41,67 +37,6 @@ export default function RootLayout({
             })(window,document,'script','dataLayer','GTM-KPNVLQ88');`,
           }}
         />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-0736D6REMC"
-        />
-        <Script
-          id="gtag-config"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){window.dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-0736D6REMC');
-            `,
-          }}
-        />
-        <meta
-          name="google-site-verification"
-          content="ran1S_cdW2B4G5g15GikF39dSLDNtlVtGtBeYFFANeY"
-        />
-        <Script
-          id="fb-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${FB_PIXEL_ID}');
-              console.log('[Meta Pixel] init ${FB_PIXEL_ID}');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-            alt=""
-          />
-        </noscript>
-        <Script
-          id="microsoft-clarity"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "wjp1e06hq0");
-            `,
-          }}
-        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -116,7 +51,6 @@ export default function RootLayout({
         </noscript>
         <ReduxProvider>
           <ToastProvider />
-          <MetaPixel />
           <ClientLayout>{children}</ClientLayout>
         </ReduxProvider>
       </body>
